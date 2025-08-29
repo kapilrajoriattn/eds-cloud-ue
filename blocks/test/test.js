4056699e06d374812a4c5faa1354f057a4e6a2c1
kapilrajoriattn <kapil.rajoria@tothenew.com>
2025-08-29 14:53:19 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,4 +1,8 @@
-export default function decorate(test) {
-  console.log("test block");
-}
+export default function decorate(block) {
+  const [child] = block.children;
 
+  // Just wrap the text in a <span> for testing
+  const span = document.createElement('span');
+  span.textContent = child.textContent + " okay!";
+  child.replaceChildren(span);
+}
